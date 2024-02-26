193cada0881bce5e48d916eade01fae712afa02f
Kostiantyn Yemets <61251118+kyemets@users.noreply.github.com>
2024-02-26 21:53:53 +0100

    added firebase & login/logout pages

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,7 +26,7 @@ const Main = () => {
     }
 
     return (
-        <div className="w-full h-[500] text-white">
+        <div className="w-full h-[550px] text-white">
             <div className="w-full h-full">
                 <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
                 <img
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,23 @@ const Home = () => {
     return (
         <div>
             <Main />
-            <Row rowID='1' title="Up Coming" fetchURL={requests.requestsUpcoming} />
-            <Row rowID='2' title="Popular" fetchURL={requests.requestPopular} />
-            <Row rowID='3' title="Trending" fetchURL={requests.requestTrending} />
-            <Row rowID='4' title="Top Rated" fetchURL={requests.requestTopRated} />
-            <Row rowID='5' title="Horror" fetchURL={requests.requestHorror} />
+            <Row
+                rowID="1"
+                title="Up Coming"
+                fetchURL={requests.requestsUpcoming}
+            />
+            <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+            <Row
+                rowID="3"
+                title="Trending"
+                fetchURL={requests.requestTrending}
+            />
+            <Row
+                rowID="4"
+                title="Top Rated"
+                fetchURL={requests.requestTopRated}
+            />
+            <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} />
         </div>
     )
 }
