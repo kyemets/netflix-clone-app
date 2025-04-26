abb932b553c18f82bec158c62ff0014b9a21d05d
Kostiantyn Yemets <61251118+kyemets@users.noreply.github.com>
2025-04-27 00:59:04 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,16 +47,6 @@ const Navbar = () => {
                 </div>
             )}
 
-            {/* <div>
-                <Link to="/login">
-                    <button className="text-white pr-4">Sign In</button>
-                </Link>
-                <Link to="/signup">
-                    <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-                        Sign Up
-                    </button>
-                </Link>
-            </div> */}
         </div>
     )
 }
